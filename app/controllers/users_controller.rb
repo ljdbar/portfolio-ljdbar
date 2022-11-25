df73421034d83b34d0e7e1f91922466543d750a4
Louis <84845571+ljdbar@users.noreply.github.com>
2022-11-25 16:25:36 +0000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(review_params)
+    @user = User.find(params[:id])
   end
 
-  def review_params
-    params.require(:user).permit(:id, :email, :password, :name, :location, :language, :availability, :bio)
-  end
+  # def user_params
+  #   params.require(:user).permit(:id, :email, :created_at, :updated_at, :password, :name, :location, :language, :availability, :bio)
+  # end
 end
